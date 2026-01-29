6caa3c4ac63d0877a5c84e6b92bb390c9e4494e7
Sridhant Dalai <sridhantrahul14@gmail.com>
2026-01-30 01:48:47 +0530

    env 2

--- api/gemini.js
+++ api/gemini.js
@@ -21,8 +21,9 @@ export default async function handler(req, res) {
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-pro",
     });
+
 
     const result = await model.generateContent(prompt);
     const text = result.response.text();
